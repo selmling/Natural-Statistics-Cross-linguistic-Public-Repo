c52c96d30f4b524d49f576b28fb8ae50d811ace0
sle64 <sle64@cornell.edu>
2023-05-15 17:30:27 -0400

    rollins cont.

--- analysis/contingency_rate.R
+++ analysis/contingency_rate.R
@@ -77,4 +77,75 @@ ggsave("../figures/contingency_rates.pdf", width = 6.47, height = 5, dpi = 1200)
 
 # ---- Rollins corpus contingency rates
 
-rand_cg_dat
+rand_dat_rollins <- rand_dat %>% 
+    filter(corpus_name == "Rollins")
+
+# ---- rate at which child vocalizations elicited Rollins caregiver speech
+
+elic_resp_rollins <- rand_dat_rollins %>%
+    filter(caregiver != "other") %>%
+    select(transcript_id, target_child_id, Language_name, caregiver, media_start, contingent) %>% 
+    group_by(transcript_id) %>% 
+    mutate(elicited_response = lead(contingent)) %>%
+    ungroup() %>% 
+    filter(caregiver == "target_child") %>% 
+    select(-c(contingent))
+
+cont_rate_rollins <- elic_resp_rollins %>%
+    group_by(transcript_id, Language_name) %>% 
+    summarize(contingency_rate = mean(elicited_response, na.rm = TRUE))
+
+cont_rate_means_rollins <- cont_rate_rollins %>% 
+    group_by(Language_name) %>% 
+    summarize(avg_contingency_rate = round(mean(contingency_rate),2))
+
+p3 <- cont_rate_rollins %>% ggplot(aes(fill = Language_name)) +
+    geom_histogram(aes(contingency_rate)) +
+    geom_vline(data=cont_rate_means_rollins,
+               aes(xintercept=avg_contingency_rate),
+               color="black", linetype="dashed", size=.6) +
+    geom_text(data=cont_rate_means_rollins,
+              aes(label = round(avg_contingency_rate, digits = 2),
+                  x = avg_contingency_rate + .25,
+                  y = 15), size = 3) +
+    coord_cartesian(ylim = c(0,25)) +
+    labs(y = "Count",
+         x = "Child vocalization contingent Rollins caregiver speech elicitation rate") +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90,vjust = 0.5, hjust = .5),
+          axis.ticks.length = unit(-2.5, "pt"),
+          legend.position = "none")
+
+# ---- rate at Rollins caregiver speech was produced contingently on child vocalization
+
+rand_cg_dat_rollins <- rand_cg_dat %>%
+    filter(corpus_name == "Rollins")
+
+cg_cont_rate_rollins <- rand_cg_dat_rollins %>%
+    group_by(transcript_id, Language_name) %>% 
+    summarize(contingency_rate = mean(contingent))
+
+cg_cont_rate_means_rollins <- cg_cont_rate_rollins %>% 
+    group_by(Language_name) %>% 
+    summarize(avg_contingency_rate = round(mean(contingency_rate),2))
+
+p4 <- cg_cont_rate_rollins %>% ggplot(aes(fill = Language_name)) +
+    geom_histogram(aes(contingency_rate)) +
+    geom_vline(data=cg_cont_rate_means_rollins,
+               aes(xintercept=avg_contingency_rate),
+               color="black", linetype="dashed", size=.6) +
+    geom_text(data=cg_cont_rate_means_rollins,
+              aes(label = round(avg_contingency_rate, digits = 2),
+                  x = avg_contingency_rate + .1,
+                  y = 15), size = 3) +
+    coord_cartesian(ylim = c(0,25)) +
+    labs(y = "Count",
+         x = "Proportion of Rollins caregiver utterances which were contingent on child vocalizations") +
+    theme_classic() +
+    theme(axis.text.x = element_text(angle = 90,vjust = 0.5, hjust = .5),
+          axis.ticks.length = unit(-2.5, "pt"),
+          legend.position = "none")
+
+plot_grid(p3, p4, ncol = 1, labels = c("A", "B"))
+
+ggsave("../figures/contingency_rates_Rollins.pdf", width = 6.47, height = 5, dpi = 1200)
