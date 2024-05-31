822a73bbb5c325efd119d4df0529ee02c7c8a701
selmling <sle64@cornell.edu>
2024-05-31 14:47:38 +0000

    random chunk filter in R

--- analysis/Tseltal.R
+++ analysis/Tseltal.R
@@ -43,33 +43,6 @@ TSE_data %>%
 # session durations are described here:
 # Elmlinger, Goldstein & Casillas, 2023 Section 2.3
 # each session 60 minutes in total
-# adding arrange line here would probably help
-
-# arrange by transcript_id and media_start
-# then group by transcript_id
-# then find the interval between rows
-# find when intervals between rows are larger than 5 minutes
-
-TSE_dur_dat <- TSE_data %>%
-    group_by(transcript_id) %>%
-    arrange(transcript_id, media_start) %>%
-    mutate(interval = media_start - lag(media_end)) %>%
-    mutate(large_interval = ifelse(is.na(interval), FALSE, interval > 450)) %>%
-    mutate(interval_index = cumsum(large_interval))
-
-# summarize durations of each intveral_index
-
-TSE_dur_sum_dat <- TSE_dur_dat %>%
-    group_by(transcript_id, interval_index) %>%
-    summarize(min = min(media_start, na.rm = TRUE),
-              max = max(media_end, na.rm = TRUE),
-              duration = max - min) %>%
-    mutate(dur_min = duration/60)
-
-TSE_dur_total_dat <- TSE_dur_sum_dat %>%
-    group_by(transcript_id) %>%
-    summarize(total_duration = sum(duration, na.rm = TRUE),
-              total_duration_min = total_duration/60)
 
 # load Tseltal time chunks
 
@@ -77,18 +50,12 @@ TSE_time_chunks <- read_csv("data/TSE_time_chunks.csv") %>%
     mutate(duration = offset - onset,
            duration_min = duration/60)
 
-# 2109 doesn't have many utterances
-# 7176 has no caregiver utterances
-
 # set seed for reproducibility
-set.seed(47)
+set.seed(74)
 
 # randomly sample 2 5-minute chunks from each transcript_id
 
-# * this needs to ensure that there are >= 5 child utterances across the two chunks
-# * this needs to ensure that there are >= 5 caregiver utterances across the two chunks
-
-TSE_time_chunks %>%
+TSE_rand_time_chunks <- TSE_time_chunks %>%
     filter(duration_min >= 5) %>% 
     group_by(filename) %>%
     sample_n(2) %>%
@@ -96,7 +63,16 @@ TSE_time_chunks %>%
     mutate(transcript_id = str_remove(filename, ".eaf"),
            media_start = onset,
            media_end = offset) %>%
-    select(transcript_id, media_start, media_end) %>%
+    select(transcript_id, media_start, media_end)
+
+# Give each chunk an index
+
+TSE_rand_time_chunks <- TSE_rand_time_chunks %>%
+    group_by(transcript_id) %>%
+    mutate(chunk_index = row_number()) %>%
+    ungroup()
+
+TSE_rand_time_chunks %>%
     write_csv("data/TSE_rand_time_chunks.csv")
     
 # ---- contingency detection
@@ -104,7 +80,7 @@ TSE_time_chunks %>%
 source_python("analysis/data_proc/contingent_extraction.py")
 
 TSE_data <- TSE_data %>% 
-    mutate(Langauge_name = "Tseltal",
+    mutate(Language_name = "Tseltal",
            Language_Family = "Mayan",
            Language_Genus = "Mayan") %>% 
     filter(!is.na(gloss))
@@ -125,31 +101,70 @@ TSE_data <- TSE_data %>%
     filter(speaker_role %in% c("Target_Child", "Mother")) %>% 
     mutate(caregiver = case_when(
                 speaker_role == "Target_Child" ~ "target_child",
-                speaker_role == "Mother" ~ "caregiver"))
+                speaker_role == "Mother" ~ "caregiver"),
+           target_child_id = transcript_id) %>%
+    arrange(transcript_id, media_start)
 
 TSE_cont_dat <- TSE_data %>% 
     assign_contingency(.,3,.001)
 
-# save to file
-# * subset to include randomly selected time chunks
-# * convert the following python code into R code:
+# ---- assign chunk_index to each row in TSE_cont_dat based on media_start and media_end
 
-TSE_dat_inc = pd.read_csv("../data/TSE_cont_dat.csv")
-TSE_rand_time_chunks = pd.read_csv("../data/TSE_rand_time_chunks.csv")
-TSE_dat_inc.rename(columns={'Langauge_name': 'Language_name'}, inplace=True)
-TSE_dat_inc['target_child_id'] = TSE_dat_inc['transcript_id']
+# Function to find chunk index based on start and end times
+check_event_in_chunks <- function(start_time, end_time, chunks) {
+  chunks %>%
+    filter(media_start <= start_time, media_end >= end_time) %>%
+    slice(1) %>%
+    pull(chunk_index) %>%
+    {if (length(.) == 0) NA else .}
+}
 
-def is_within_time_chunk(transcript):
-    transcript_id, transcript_rows = transcript
-    time_chunks = TSE_rand_time_chunks[TSE_rand_time_chunks['transcript_id'] == transcript_id]
-    mask = transcript_rows.apply(lambda row: any(time_chunk['media_start'] <= row['media_start'] and time_chunk['media_end'] >= row['media_end'] for _, time_chunk in time_chunks.iterrows()), axis=1)
-    return transcript_rows[mask]
+# nest data by transcript_id
+TSE_cont_dat_nest <- TSE_cont_dat %>%
+  group_by(transcript_id) %>%
+  nest()
 
-TSE_dat_inc_grouped = TSE_dat_inc.groupby('transcript_id')
+# nest time chunks by transcript_id
+TSE_rand_time_chunk_nest <- TSE_rand_time_chunks %>%
+  group_by(transcript_id) %>%
+  nest()
 
-TSE_rand_dat_inc = pd.concat([is_within_time_chunk(transcript) for transcript in TSE_dat_inc_grouped])
+# merge nests
+TSE_rand_dat <- TSE_cont_dat_nest %>%
+  left_join(TSE_rand_time_chunk_nest, by = "transcript_id", suffix = c("", "_chunks"))
 
-# ----
+# denote chunk index for each row
+TSE_rand_dat <- TSE_rand_dat %>%
+  mutate(data = map2(data, data_chunks, ~ .x %>%
+         rowwise() %>%
+         mutate(chunk_index = check_event_in_chunks(media_start, media_end, .y)) %>%
+         ungroup())) %>%
+  select(-data_chunks) %>%
+  unnest(data)
+
+# filter to keep only rows with chunk index
+TSE_rand_dat <- TSE_rand_dat %>%
+  filter(!is.na(chunk_index))
+
+# ---- check for sufficient utterances
+
+# at least 5 child utterances
+TSE_rand_dat %>%
+    group_by(transcript_id) %>% nest() %>%
+    mutate(sufficient_utterances = map_lgl(data, ~sufficient_child_utterances(.x, 5)))
+
+# at least 5 caregiver utterances
+TSE_rand_dat %>%
+    group_by(transcript_id) %>% nest() %>%
+    mutate(sufficient_utterances = map_lgl(data, ~sufficient_caregiver_utterances(.x, 5)))
+
+# take new time chunks where sufficient_utterances == False
+
+TSE_rand_time_chunks_new <- TSE_rand_time_chunks %>%
+    filter(transcript_id %in% TSE_rand_dat$transcript_id) %>%
+    anti_join(TSE_rand_dat %>% filter(sufficient_utterances == TRUE), by = "transcript_id")
+
+# ---- save to file
 
 TSE_cont_dat %>% 
     write_csv("data/TSE_cont_dat.csv")
